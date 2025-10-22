763e5e7e1900cdb1696f82d0ad98db2bf982d272
Caio Augusto <caio_augusto02@hotmail.com>
2025-10-22 20:45:25 -0300

    Integrando o relatório para o Cypress Cloud

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: "egsc4m",
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
