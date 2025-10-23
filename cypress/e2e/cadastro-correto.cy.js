7f263c46c2e8e45aca1bd5d86e9b7db916dca1d1
Caio Augusto <caio_augusto02@hotmail.com>
2025-10-22 21:12:28 -0300

    Criação do comando personalizado para teste do login

--- cypress/e2e/cadastro-correto.cy.js
+++ cypress/e2e/cadastro-correto.cy.js
@@ -1,13 +1,9 @@
-
-describe("Página de Cadastri", () => {
-  
-  it("Deve preencher os campos do formulário corretamente para cadastrar um novo usuário", () => {
-    cy.visit("https://adopet-frontend-cypress.vercel.app");
-    cy.get('[data-test="register-button"]').click();
-    cy.get('[data-test="input-name"]').type("Caio Augusto");
-    cy.get('[data-test="input-email"]').type("caio_augusto02@hotmail.com");
-    cy.get('[data-test="input-password"]').type("Caio1234");
-    cy.get('[data-test="input-confirm-password"]').type("Caio1234");
-    cy.get('[data-test="submit-button"]').click();
-  });
-});
+describe('Página de cadastro', () => {
+    beforeEach(() =>{
+        cy.visit('https://adopet-frontend-iota.vercel.app/');
+        cy.get('[data-test="register-button"]').click();
+    })   
+        it('Deve preencher os campos do cadastro corretamente e clicar no botão cadastrar', () => {
+            cy.cadastrar('Marjorie Anoquevem','marjorie@email.com', 'Senha123')  
+    })
+})
